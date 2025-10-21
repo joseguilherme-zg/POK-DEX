58d346e75b0e39ff3fcb1f1ba25f03dd3dcf9832
joseguilherme-zg <zansavio.goncalves@escola.pr.gov.br>
2025-10-21 08:24:47 -0300

    update

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ const pokemon_input = document.getElementById("input");
 
 const pokemon_prev = document.getElementById("bnt-prev");
 const pokemon_next = document.getElementById("bnt-next");
+
+let searchPokemon = 1;
+
+const fetchpokemon = async (pokemon) => {
+    const APIResponse = wait fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if(APIResponse.status ===200){
+        const data = await APIResponse.json();
+        return data;
+    }
+}
